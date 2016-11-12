dfb3e0d95781335b0ae08353f8f7f57931455f59
Boris Borisov <moisko@gmail.com>
2016-11-12 09:46:39 +0200

    remove css style section from follow me component

--- src/app/followme/followme.component.ts
+++ src/app/followme/followme.component.ts
@@ -2,8 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "follow-me",
-  templateUrl: "./followme.component.html",
-  styles: ["./followme.component.css"]
+  templateUrl: "./followme.component.html"
 })
 export class FollowMeComponent {
 
