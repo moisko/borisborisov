9b42c677117a3674d3f919953eea8aadc91f0c90
Boris Borisov <moisko@gmail.com>
2016-11-12 09:39:05 +0200

    change github icon form iframe to anchor tag add css style for follow me section

--- src/app/followme/followme.component.ts
+++ src/app/followme/followme.component.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "follow-me",
-  templateUrl: "./followme.component.html"
+  templateUrl: "./followme.component.html",
+  styles: ["./followme.component.css"]
 })
 export class FollowMeComponent {
 
